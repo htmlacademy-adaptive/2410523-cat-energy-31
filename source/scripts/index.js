cb4685e5918e499665e8d971f6e13ad5dea786c0
Марат Замалетдинов <148064047+Zamlet97@users.noreply.github.com>
2024-06-07 10:19:06 +0400

    Финал

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,5 +6,3 @@ const map = document.querySelector('.contacts__map');
 navButton.classList.remove('navigation__button-close--nojs');
 navDiv.classList.remove('navigation__menu--nojs');
 map.classList.remove('contacts__map--nojs');
-
-
